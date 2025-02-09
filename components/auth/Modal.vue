cadfec272bf807b3ce352acb88996668ca72bc4f
Juaco Sanchez <jasanchez1@uc.cl>
2025-02-09 13:17:01 +0100

    fix confirm password

--- components/auth/Modal.vue
+++ components/auth/Modal.vue
@@ -80,7 +80,7 @@
             v-model="form.passwordConfirm"
             type="password"
             required
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500"
+            class="mt-2 block w-full rounded-md border-gray-300 px-4 py-3 text-base shadow-sm focus:border-primary-500 focus:ring-primary-500"
           />
         </div>
 
